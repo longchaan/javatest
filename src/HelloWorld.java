5ca12ab08ceffb5bc02cf87b826b426853cebfe6
Yongchan Kim <longchan@nara.co.kr>
2016-06-28 21:26:48 +0900

    I hello you ~~

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -4,7 +4,7 @@ public class HelloWorld {
 	public static void main(String[] args) throws Exception {
 		System.out.println("Hello World");
 		System.out.println("Java World");
-		System.out.println("I hello you");
+		System.out.println("I hello you ~~");
 	}
 
 }
